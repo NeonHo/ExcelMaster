9f6ab1de0f3c7d6314f1d36e57c8e0db7828cafe
Neon <1123151343@qq.com>
2021-07-13 15:17:35 +0800

    initiate column_info_list.

--- TestMain.py
+++ TestMain.py
@@ -9,6 +9,10 @@ def main():
     sheet_index = eval(input("Enter the sheet you want to process:"))
     sheet = table.sheets()[sheet_index]
     excel_sheet_master = ExcelSheetMaster(sheet)
+    column_info_list = [{'column_index': 1, 'min_border': 0.195, 'max_border':  0.485,
+                         'include_min': False, 'include_max': True},
+                        {'column_index': 2, 'min_border': 2.545, 'max_border': 4.505,
+                         'include_min': False, 'include_max': True}]
     excel_sheet_master.is_all_columns_elements_in_range()
 
 
