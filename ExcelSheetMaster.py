076816e9de7da4a242e8368f7756992ac8e050d8
Neon <1123151343@qq.com>
2021-07-13 17:34:29 +0800

    calculate validate RandomForest.

--- ExcelSheetMaster.py
+++ ExcelSheetMaster.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 
+# pip3 install xlrd==1.2.0
+
 class ExcelSheetMaster:
     def __init__(self, sheet):
         self.sheet = sheet
@@ -16,7 +18,7 @@ class ExcelSheetMaster:
         :param include_max:
         :return: bool
         """
-        value = self.sheet.cell_value(row_index, column_index)
+        value = float(self.sheet.cell_value(row_index, column_index))
         if include_min:
             if include_max:
                 if min_border <= value <= max_border:
@@ -40,21 +42,23 @@ class ExcelSheetMaster:
                 else:
                     return False
 
-    def is_all_columns_elements_in_range(self, column_info_list: list):
+    def is_all_columns_elements_in_range(self, column_info_list: list, begin_row_index: int):
         """
         If all column is in their own range? Give an array with value:(0, 1)
+        :param begin_row_index: the first row we count.
         :param column_info_list: [column_index, min_border, max_border, include_min: bool, include_max: bool]
         :return: an array with value:(0, 1) 1=in range, 0=not in range.
         """
-        res_array = np.zeros(shape=(self.sheet.nrows, self.sheet.ncolumns), dtype=int)
+        res_array = np.zeros(shape=(self.sheet.nrows, self.sheet.ncols), dtype=int)
         for column_info in column_info_list:
             for row_index in range(self.sheet.nrows - 1):
-                res = self.is_in_range(row_index=row_index,
-                                       column_index=column_info['column_index'],
-                                       min_border=column_info['min_border'],
-                                       max_border=column_info['max_border'],
-                                       include_min=column_info['include_min'],
-                                       include_max=column_info['include_max'])
-                if res:
-                    res_array[row_index][column_info['column_index']] = 1
+                if row_index >= begin_row_index:
+                    res = self.is_in_range(row_index=row_index,
+                                           column_index=column_info['column_index'],
+                                           min_border=column_info['min_border'],
+                                           max_border=column_info['max_border'],
+                                           include_min=column_info['include_min'],
+                                           include_max=column_info['include_max'])
+                    if res:
+                        res_array[row_index][column_info['column_index']] = 1
         return res_array
--- TestMain.py
+++ TestMain.py
@@ -1,19 +1,33 @@
+import numpy as np
 import xlrd
 
 from ExcelSheetMaster import ExcelSheetMaster
 
 
 def main():
-    file_name = input("Enter the excel file Absolute Path:")
+    file_name = input("Enter the excel file Absolute Path:")  # E:\DraftBox\data2333.xlsx
     table = xlrd.open_workbook(file_name)
     sheet_index = eval(input("Enter the sheet you want to process:"))
     sheet = table.sheets()[sheet_index]
     excel_sheet_master = ExcelSheetMaster(sheet)
-    column_info_list = [{'column_index': 1, 'min_border': 0.195, 'max_border':  0.485,
+    column_info_list = [{'column_index': 2, 'min_border': 0.485, 'max_border': 100,
                          'include_min': False, 'include_max': True},
-                        {'column_index': 2, 'min_border': 2.545, 'max_border': 4.505,
-                         'include_min': False, 'include_max': True}]
-    excel_sheet_master.is_all_columns_elements_in_range()
+                        {'column_index': 1, 'min_border': 0, 'max_border': 5.185,
+                         'include_min': True, 'include_max': True}]
+    res_array = excel_sheet_master.is_all_columns_elements_in_range(column_info_list=column_info_list, begin_row_index=1)
+    res_array1 = np.sum(res_array, axis=1)
+    res_array2 = res_array1[res_array1 == 2]
+    column_info_list = [{'column_index': 2, 'min_border': 0.485, 'max_border': 100,
+                         'include_min': False, 'include_max': True},
+                        {'column_index': 1, 'min_border': 0, 'max_border': 5.185,
+                         'include_min': True, 'include_max': True},
+                        {'column_index': 4, 'min_border': 1, 'max_border': 1,
+                         'include_min': True, 'include_max': True},
+                        ]
+    res_array_3 = excel_sheet_master.is_all_columns_elements_in_range(column_info_list=column_info_list, begin_row_index=1)
+    res_array_4 = np.sum(res_array_3, axis=1)
+    res_array_5 = res_array_4[res_array_4 == 3]
+    pass
 
 
 if __name__ == '__main__':
