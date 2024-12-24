bc8868c96a1c853928dc5b08a0596a3fef393fde
NeonHo <1123151343@qq.com>
2024-12-24 22:31:48 +0800

    BuildExcel from MarkDown.

---
+++ BuildExcel.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+
+
+# write a function to read the csv file, if there is only one row or one column, return a list of the data, otherwise return and an empty list.
+def read_csv(file_path: str):
+    """read the csv file, if there is only one row or one column, return a list of the data, otherwise return and an empty list.
+
+    Args:
+        file_path (str): the path of the csv file.
+
+    Returns:
+        list : a list of the data in the csv file.
+    """
+    data = pd.read_csv(file_path)
+    if data.shape[0] == 1:
+        return data.values.tolist()[0]
+    elif data.shape[1] == 1:
+        return data.values.tolist()
+    else:
+        return []
+    
+# Convert table data in Markdown format to Excel format.
+def markdown_to_excel(markdown: str, file_path: str):
+    """Convert table data in Markdown format to Excel format.
+
+    Args:
+        markdown (str): the table data in Markdown format.
+        file_path (str): the path of the Excel file.
+
+    Returns:
+        None
+    """
+    # Split the markdown data into rows.
+    rows = markdown.strip().split('\n')
+    # Split the rows into columns.
+    # If the elements in the row are '----', it means the previous row is the header.
+    if rows[1][1:5] == '----':
+        header = rows[0].split('|')[1:-1]
+        rows = rows[2:]
+    # The following rows are the data.
+    data = [row.split('|')[1:-1] for row in rows]
+    # Create a DataFrame.
+    df = pd.DataFrame(data, columns=header)
+    # Save the DataFrame to an Excel file.
+    df.to_excel(file_path, index=False)
+    
+    
--- TestMain.py
+++ TestMain.py
@@ -1,34 +1,14 @@
-import numpy as np
-import xlrd
-
-from ExcelSheetMaster import ExcelSheetMaster
+from BuildExcel import markdown_to_excel
 
 
 def main():
-    file_name = input("Enter the excel file Absolute Path:")  # E:\DraftBox\data2333.xlsx
-    table = xlrd.open_workbook(file_name)
-    sheet_index = eval(input("Enter the sheet you want to process:"))
-    sheet = table.sheets()[sheet_index]
-    excel_sheet_master = ExcelSheetMaster(sheet)
-    column_info_list = [{'column_index': 2, 'min_border': 0.485, 'max_border': 100,
-                         'include_min': False, 'include_max': True},
-                        {'column_index': 1, 'min_border': 0, 'max_border': 5.185,
-                         'include_min': True, 'include_max': True}]
-    res_array = excel_sheet_master.is_all_columns_elements_in_range(column_info_list=column_info_list, begin_row_index=1)
-    res_array1 = np.sum(res_array, axis=1)
-    res_array2 = res_array1[res_array1 == 2]
-    column_info_list = [{'column_index': 2, 'min_border': 0.485, 'max_border': 100,
-                         'include_min': False, 'include_max': True},
-                        {'column_index': 1, 'min_border': 0, 'max_border': 5.185,
-                         'include_min': True, 'include_max': True},
-                        {'column_index': 4, 'min_border': 1, 'max_border': 1,
-                         'include_min': True, 'include_max': True},
-                        ]
-    res_array_3 = excel_sheet_master.is_all_columns_elements_in_range(column_info_list=column_info_list, begin_row_index=1)
-    res_array_4 = np.sum(res_array_3, axis=1)
-    res_array_5 = res_array_4[res_array_4 == 3]
-    pass
-
+    # Read MarkDown File cities-rads.md and Convert to Excel File cities-rads.xlsx
+    markdown_path = "DataSet/cities-rads.md"
+    excel = "DataSet/cities-rads.xlsx"
+    # Read the markdown file to string.
+    with open(markdown_path, 'r') as f:
+        markdown = f.read()
+    markdown_to_excel(markdown, excel)
 
 if __name__ == '__main__':
     main()
